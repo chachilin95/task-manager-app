c276251c2a23926ea02b4481e59b60682ba04da0
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-08-26 17:24:25 -0500

    Created form for login component

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Login: React.FC = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        console.log(email);
+        console.log(password);
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    type='text'
+                    placeholder='ex. anonymous@example.com'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    name='email'/>
+                <input
+                    type='password'
+                    placeholder='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    name='password'/>
+                <button>Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
